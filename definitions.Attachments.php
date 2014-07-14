fc4ac5b8d427b0a4b486f88c08a20c59f75a0a6f
Martin Mårtensson <martin@redward.se>
2014-07-14 09:24:55 +0200

    Added translation for Attachments plugin

---
+++ definitions.Attachments.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Language pack for the Attachments plugin
+ * https://github.com/esotalk/Attachments
+ **/
+$definitions["Attach a file"] = "Bifoga en fil";
+$definitions["Drop files to upload"] = "Släpp filer här för att ladda upp";
+$definitions["Embed in post"] = "Infoga i inlägget";
+$definitions["message.attachmentNotFound"] = "Den bifogade filen kan inte visas. Det kan vara så att filen inte finns eller så har du inte rättighet att se filen.";
