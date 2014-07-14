ba0bc8fd1a5cb6274c6c3518d7a6a60413043b26
Martin Mårtensson <martin@redward.se>
2014-07-14 09:21:39 +0200

    Added translation for the Likes plugin

---
+++ definitions.Likes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Language pack for the Likes plugin
+ * https://github.com/esotalk/Likes
+ **/
+$definitions["Unlike"] = "Ångra gilla";
+$definitions["Like"] = "Gilla";
+$definitions["Members Who Liked This Post"] = "Medlemmar som gillar detta inlägg";
+$definitions["%s likes this."] = "%s gillar detta.";
+$definitions["%s like this."] = "%s gillar detta.";
