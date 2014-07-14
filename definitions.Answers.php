cfe2a390f4f93edfe5d381af515ebbbbc12f2828
Martin Mårtensson <martin@redward.se>
2014-07-14 10:10:31 +0200

    Added translations for Answers and Views plugin

---
+++ definitions.Answers.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Language pack for the Answers plugin
+ * https://github.com/esotalk/Answers
+ **/
+$definitions["Answer"] = "Svar";
+$definitions["Answered"] = "Svarad";
+$definitions["Answered by %s"] = "Svarad av %s";
+$definitions["label.answered"] = "Svarad";
+$definitions["Remove answer"] = "Ta bort svar";
+$definitions["See post in context"] = "Se inläggets innehåll";
+$definitions["This post answered my question"] = "Detta inlägg svarade min fråga";
---
+++ definitions.Views.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Language pack for the Views plugin
+ * https://github.com/esotalk/Views
+ **/
+$definitions["gambit.order by views"] = "sorttera på visningar";
+$definitions["%s view"] = "%s visning";
+$definitions["%s views"] = "%s visningar";
