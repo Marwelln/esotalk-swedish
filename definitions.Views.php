09f451d9dc537b006765a807012d02a635cd4547
Martin Mårtensson <Marwelln@gmail.com>
2014-08-29 15:22:37 +0200

    Fixed typo

--- definitions.Views.php
+++ definitions.Views.php
@@ -3,6 +3,6 @@
  * Language pack for the Views plugin
  * https://github.com/esotalk/Views
  **/
-$definitions["gambit.order by views"] = "sorttera på visningar";
+$definitions["gambit.order by views"] = "sortera på visningar";
 $definitions["%s view"] = "%s visning";
 $definitions["%s views"] = "%s visningar";
